ef08d300e3d6189e135f166ab2743f954d4a2872
PetiZS <zsigripeter2011@gmail.com>
2024-11-03 18:30:59 +0100

    I WARNED YOU

--- IntenseFlashing_EPILIPSY_WARNING/IM_NOT_RESPONSIBLE_FOR_ANY_DAMAGES.lua
+++ IntenseFlashing_EPILIPSY_WARNING/IM_NOT_RESPONSIBLE_FOR_ANY_DAMAGES.lua
@@ -1,7 +1,13 @@
 local ColorCorrection = Instance.new("ColorCorrectionEffect",game.Lighting)
 
-game["Run Service"].RenderStepped:Connect(function()
+local RunService = game["Run Service"].RenderStepped:Connect(function()
 	ColorCorrection.Contrast = math.random(-100/10,100/10)
 	ColorCorrection.Saturation = math.random(-100/10,100/10)
 	ColorCorrection.TintColor = Color3.new(math.random(),math.random(),math.random())
 end)
+
+task.wait(30)
+
+RunService:Disconnect()
+
+ColorCorrection:Destroy()
